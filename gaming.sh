20fc42c7f0833b0a8095bd65e308f9bb6f4625ff
Hack-Net998 <77234932+Hack-Net998@users.noreply.github.com>
2021-01-10 21:19:32 +0700

    Add files via upload

---
+++ gaming.sh
@@ -0,0 +1,58 @@
+clear  
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1                                                      
+figlet Hack | lolcat
+sleep 1
+figlet Net | lolcat
+sleep 1
+figlet Wooii | lolcat
+sleep 2
+clear
+sleep 3
+echo $blue "================================"
+sleep 1
+echo $red "Author =Mr.Wahyu"
+sleep 2
+echo "Channel Youtube:Hack Net"
+sleep 2
+echo "DAN SUBSCRIBE Channel Hack Net"
+sleep 2
+echo "Facebook:Dafit Gaming"                               
+sleep 2
+echo $blue "================================"
+sleep 8
+figlet -f small "Hack Net"
+echo "(1)Hack Fb Login Via Cookie"
+sleep 1
+echo "(2)Install bahan dulu cok"
+sleep 1
+echo "(3)Exit"
+sleep 1
+read -p "pilih nomor: " input;
+if [ $input = 1 ]
+then
+clear
+git clone https://github.com/haniefaktivisme/fbcrack.git
+cd fbcrack
+ls
+python crack.py
+cowok
+fi
+if [ $input = 2 ]
+then
+clear
+pkg update;pkg upgrade;pkg install python2;pkg install git;pip2 install requests;pip2 install mechanize;git clone https://github.com/aimscar/panenakunfb.sh.git;ls;cd panenakunfb.sh;ls;bash panenakunfb.sh;cowok;Tripel666
+fi
+if [ $input = 3 ]
+then
+clear
+echo " tunggu sebentar lagi coli...."
+sleep 3
+exit
+fi
